ca4a48a7179da436d9a8f7c27db2ab03b22f8a5c
SS-Cat <ivensjorisnbacon@gmail.com>
2019-05-14 01:10:42 -0300

    Inserção e Remoção ordenada de Cartas na mão

---
+++ Player/Player.c
@@ -0,0 +1,56 @@
+#include "Player.h"
+
+void printHand(Player player)
+{
+	for (int i = 0; i < player.handCount; i++)
+	{
+		printCard(player.hand[i]);
+	}
+}
+
+bool addToHand(Player *player, Card card)
+{
+	if (player->handCount == 4)
+		return false;
+	int i;
+	for (i = player->handCount - 1; i >= 0 && player->hand[i].faceNumber > card.faceNumber; i--)
+		player->hand[i + 1] = player->hand[i];
+
+	player->hand[i + 1] = card;
+	player->handCount++;
+	return true;
+}
+
+bool removeOfHand(Player *player, Card card)
+{
+	if (player->handCount == 0)
+		return false;
+
+	int left = 0;
+	int right = player->handCount;
+	int mid;
+	int result = -1;
+	while (left < right)
+	{
+		mid = (left + right) / 2;
+		if (player->hand[mid].faceNumber == card.faceNumber)
+		{
+			result = mid;
+			break;
+		}
+		else if (card.faceNumber > player->hand[mid].faceNumber)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	if (result == -1)
+		return false;
+
+	int i;
+	for (i = result; i < player->handCount; i++)
+		player->hand[i] = player->hand[i + 1];
+
+	player->handCount--;
+
+	return true;
+}
--- Player/Player.h
+++ Player/Player.h
@@ -3,12 +3,16 @@
 
 typedef struct player
 {
-    CardStack *cardStack;
-    int handCount;
-    Card hand[];
+	CardStack *cardStack;
+	int handCount;
+	Card hand[4];
 } Player;
 
 void printHand(Player player);
+
+bool addToHand(Player *player, Card card);
+
+bool removeOfHand(Player *player, Card card);
 
 // void handRemoveItem(Player player, Board board, int index);
 
