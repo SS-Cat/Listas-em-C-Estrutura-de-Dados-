0d86f4c727504daadafd73594852eb0bcaa6c7bd
SS-Cat <ivensjorisnbacon@gmail.com>
2019-05-14 01:10:53 -0300

    Testes comentados

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "Cards/DeckofCards.h"
 #include "Player/CardStack.h"
+#include "Player/Player.h"
 
 #define RED "\x1B[31m"
 #define GRN "\x1B[32m"
@@ -29,19 +30,46 @@ int main(void)
 		pushCardNode(&stack2, deck.cards[i + 11]);
 
 	mergeStacks(&stack1, &stack2);
-	// for (int i = 0; i < deck.numberOfCards + 1; i++)
-	// {
-	// 	Card card;
-	// 	dealCard(&deck, &card);
-	// 	printCard(card);
-	// }
 
-	// for (int i = 0; i < deck.numberOfCards; i++)
-	// {
-	// 	if (i % 2 == 0)
-	// 		printf(GRN BGYEL "%s of %s\n" RESET, deck.cards[i].faceName, deck.cards[i].suit);
-	// 	else
-	// 		printf(YEL BGGRN "%s of %s\n" RESET, deck.cards[i].faceName, deck.cards[i].suit);
-	// }
+	printCard(stack1.bottom->card);
+
+	Player jogador1;
+	jogador1.handCount = 0;
+	Card card;
+	dealCard(&deck, &card);
+	addToHand(&jogador1, card);
+	dealCard(&deck, &card);
+	addToHand(&jogador1, card);
+	dealCard(&deck, &card);
+	addToHand(&jogador1, card);
+	dealCard(&deck, &card);
+	addToHand(&jogador1, card);
+
+	printHand(jogador1);
+
+	removeOfHand(&jogador1, card);
+	removeOfHand(&jogador1, card);
+
+	printHand(jogador1);
+
+	/*
+	* teste para o comando dealCard
+	for (int i = 0; i < deck.numberOfCards + 1; i++)
+	{
+		Card card;
+		dealCard(&deck, &card);
+		printCard(card);
+	}
+	*/
+	/*
+	* teste para vê se deck foi preenxido corretamente
+	for (int i = 0; i < deck.numberOfCards; i++)
+	{
+		if (i % 2 == 0)
+			printf(GRN BGYEL "%s of %s\n" RESET, deck.cards[i].faceName, deck.cards[i].suit);
+		else
+			printf(YEL BGGRN "%s of %s\n" RESET, deck.cards[i].faceName, deck.cards[i].suit);
+	}
+	*/
 	return 0;
 }
